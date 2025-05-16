16fb0658adcb2f78ad0b36e3f0c8d71861d36f64
Saurabh <saurabhayush2001@gmail.com>
2025-05-16 17:38:07 +0000

    CRUD Operation in nodeJS

---
+++ index.js
@@ -0,0 +1,169 @@
+const express = require('express')
+const path = require('path')
+
+const {open} = require('sqlite')
+const sqlite3 = require('sqlite3')
+
+const app = express()
+//this identify the json request
+app.use(express.json())
+
+const dbPath = path.join(__dirname, 'goodreads.db')
+
+let db = null
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    })
+    app.listen(3000, () => {
+      console.log('Server Running at http://localhost:3000/')
+    })
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`)
+    process.exit(1)
+  }
+}
+initializeDBAndServer()
+
+// Get Books API
+app.get('/books/', async (request, response) => {
+  const getBooksQuery = `
+    SELECT
+      *
+    FROM
+      book
+    ORDER BY
+      book_id;`
+  const booksArray = await db.all(getBooksQuery)
+  response.send(booksArray)
+})
+
+//Get Book API
+app.get('/books/:bookId/', async (request, response) => {
+  //to get the id of request parameter
+  const {bookId} = request.params
+  const getBooksQuery = `
+    SELECT
+      *
+    FROM
+      book
+    WHERE 
+      book_id = ${bookId};`
+  // db.get() this will used when we need only one row
+  const booksArray = await db.get(getBooksQuery)
+  response.send(booksArray)
+})
+
+//create a new book
+app.post('/books/', async (request, response) => {
+  const bookDetails = request.body
+  const {
+    title,
+    authorId,
+    rating,
+    ratingCount,
+    reviewCount,
+    description,
+    pages,
+    dateOfPublication,
+    editionLanguage,
+    price,
+    onlineStores,
+  } = bookDetails
+
+  const addBookQuery = `
+    INSERT INTO
+      book (title,author_id,rating,rating_count,review_count,description,pages,date_of_publication,edition_language,price,online_stores)
+    VALUES
+      (
+        '${title}',
+         ${authorId},
+         ${rating},
+         ${ratingCount},
+         ${reviewCount},
+        '${description}',
+         ${pages},
+        '${dateOfPublication}',
+        '${editionLanguage}',
+         ${price},
+        '${onlineStores}'
+      );`
+  const dbResponse = await db.run(addBookQuery)
+  console.log(dbResponse)
+  const bookId = dbResponse.lastID
+  response.send({bookId: bookId})
+})
+
+//update book API
+
+app.put('/books/:bookId/', async (request, response) => {
+  const {bookId} = request.params
+
+  const bookDetails = request.body
+
+  const {
+    title,
+    authorId,
+    rating,
+    ratingCount,
+    reviewCount,
+    description,
+    pages,
+    dateOfPublication,
+    editionLanguage,
+    price,
+    onlineStores,
+  } = bookDetails
+
+  const updateBookQuery = `
+    UPDATE
+      book
+    SET
+      title='${title}',
+      author_id=${authorId},
+      rating=${rating},
+      rating_count=${ratingCount},
+      review_count=${reviewCount},
+      description='${description}',
+      pages=${pages},
+      date_of_publication='${dateOfPublication}',
+      edition_language='${editionLanguage}',
+      price= ${price},
+      online_stores='${onlineStores}'
+    WHERE
+      book_id = ${bookId};`
+  await db.run(updateBookQuery)
+  response.send('Book Updated Successfully!!!')
+})
+// DELETE Book API
+
+app.delete('/books/:bookId/', async (request, response) => {
+  const {bookId} = request.params
+
+  const deleteBookQuery = `
+    DELETE FROM
+        book
+    WHERE
+        book_id = ${bookId};`
+
+  await db.run(deleteBookQuery)
+  response.send(`Book with ID ${bookId} is deleted`)
+})
+
+app.get('/authors/:authorId/books/', async (request, response) => {
+  //to get the id of request parameter
+  const {authorId} = request.params
+  const getAuthorBooksQuery = `
+    SELECT
+    *
+    FROM
+        book
+    WHERE
+        author_id = ${authorId};`
+  // db.get() this will used when we need only one row
+  const booksArray = await db.get(getAuthorBooksQuery)
+  response.send(booksArray)
+})
